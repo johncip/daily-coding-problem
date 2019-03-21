7d4612f7c5a1067725d748bf52ad35e8b9592437
John Cipriano <johnmikecip@gmail.com>
2019-03-20 23:55:59 -0700

    rearrange specs

--- spec/arrays/1_product_of_others_spec.rb
+++ spec/arrays/1_product_of_others_spec.rb
@@ -1,28 +1,28 @@
 require './arrays/1_product_of_others'
 
-describe 'product_of_all_other_elements' do
-  versions = [
-    lambda { |x| product_of_other_els_temp(x) },
-    lambda { |x| product_of_other_els_div(x) },
-    lambda { |x| product_of_other_els_final(x) }
-  ]
+versions = [
+  'product_of_other_els_temp',
+  'product_of_other_els_div',
+  'product_of_other_els_final'
+]
 
-  versions.each do |fn|
+versions.each do |name|
+  describe name do
     it 'works on a few' do
-      expect(fn.call [3, 2, 1]).to eq [2, 3, 6]
+      expect(send name, [3, 2, 1]).to eq [2, 3, 6]
     end
 
     it 'works on a few more' do
-      expect(fn.call [1, 2, 3, 4, 5]).to eq [120, 60, 40, 30, 24]
+      expect(send name, [1, 2, 3, 4, 5]).to eq [120, 60, 40, 30, 24]
     end
 
     # from me
     it 'works for a single-element array' do
-      expect(fn.call [2]).to eq [1]
+      expect(send name, [2]).to eq [1]
     end
 
     it 'works for a double-element array' do
-      expect(fn.call [2, 3]).to eq [3, 2]
+      expect(send name, [2, 3]).to eq [3, 2]
     end
   end
 end
--- spec/arrays/2_smallest_sort_window_spec.rb
+++ spec/arrays/2_smallest_sort_window_spec.rb
@@ -1,14 +1,18 @@
 require './arrays/2_smallest_sort_window'
 
-describe 'smallest_sort_window' do
-  versions = [
-    lambda { |x| smallest_sort_window_1(x) },
-    lambda { |x| smallest_sort_window_2(x) },
-  ]
+versions = [
+  'smallest_sort_window_1',
+  'smallest_sort_window_2',
+]
 
-  versions.each do |fn|
+versions.each do |name|
+  describe name do
     it 'works on the book example' do
-      expect(fn.call [3, 7, 5, 6, 9]).to eq [1, 3]
+      expect(send name, [3, 7, 5, 6, 9]).to eq [1, 3]
+    end
+
+    it 'works on a sorted array' do
+      expect(send name, [1, 2, 3]).to eq [nil, nil]
     end
   end
 end
